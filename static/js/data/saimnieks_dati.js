d50574cf757e0e9558003ddc13eb466146e4a1b6
ievahaa <65168494+ievahaa@users.noreply.github.com>
2021-02-22 18:29:38 +0200

    izdzēsta html tabula

--- static/js/data/saimnieks_dati.js
+++ static/js/data/saimnieks_dati.js
@@ -1,8 +1,8 @@
 function tableFromJson() {
 
-    var col = [];
-    for (var i = 0; i < users.length; i++) {
-        for (var key in users[i]) {
+    let col = [];
+    for (let i = 0; i < users.length; i++) {
+        for (let key in users[i]) {
             if (col.indexOf(key) === -1) {
                 col.push(key);
             }
@@ -10,28 +10,28 @@ function tableFromJson() {
     }
 
     // Create a table.
-    var table = document.createElement("table");
+    let table = document.createElement("table");
     table.setAttribute('class', 'tabula');
 
     // Create table header row using the extracted headers above.
-    var tr = table.insertRow(-1);                   // table row.
+    let tr = table.insertRow(-1);                   // table row.
     let virsraksti = ["VĀRDS", "UZVĀRDS", "PAROLE", "LOMA", "LABOT"];
 
-    for (var i = 0; i < virsraksti.length; i++) {
-        var th = document.createElement("th");      // table header.
+    for (let i = 0; i < virsraksti.length; i++) {
+        let th = document.createElement("th");      // table header.
         th.innerHTML = virsraksti[i];
         tr.appendChild(th);
         th.setAttribute('class', 'pirmarinda kolona')
     }
 
-    for (var i = 1; i < users.length; i++) {
+    for (let i = 1; i < users.length; i++) {
 
         tr = table.insertRow(-1);
         tr.setAttribute('class', 'visasrindas kolona');
 
-        for (var j = 1; j < col.length + 1; j++) {
+        for (let j = 1; j < col.length + 1; j++) {
             if (j == 4) {
-                var list = "<select class='lomatab'>" +
+                let list = "<select class='lomatab'>" +
                     "<option selected>" + roles[users[i][col[j]] - 1] + "<\/option>" +
                     "<option value=\"1\">Saiminieks<\/option>" +
                     "<option value=\"2\">Darbinieks<\/option>" +
@@ -40,17 +40,17 @@ function tableFromJson() {
                     "<option value=\"5\">Dežurante-apkopēja<\/option>" +
                     "<option value=\"6\">Skolotājs<\/option>" +
                     "<\/select>";
-                var tabCell = tr.insertCell(-1);
+                let tabCell = tr.insertCell(-1);
                 tabCell.innerHTML = list;
             }
             else if (j == 5) {
-                var labot = "<button id=" + i + "><img src='./static/images/labot.svg'></button>"
-                var dzest = "<button id=" + i + "><img src='./static/images/dzest.svg'></button>"
-                var tabCell = tr.insertCell(-1);
+                const labot = "<button id=" + i + "><img src='./static/images/labot.svg'></button>"
+                const dzest = "<button id=" + i + "><img src='./static/images/dzest.svg'></button>"
+                let tabCell = tr.insertCell(-1);
                 tabCell.innerHTML = labot + " " + dzest;
             }
             else {
-                var tabCell = tr.insertCell(-1);
+                let tabCell = tr.insertCell(-1);
                 tabCell.innerHTML = users[i][col[j]];
             }
 
@@ -58,7 +58,7 @@ function tableFromJson() {
     }
 
     // Now, add the newly created table with json data, to a container.
-    var divShowData = document.getElementById('showData');
+    const divShowData = document.getElementById('showData');
     divShowData.innerHTML = "";
     divShowData.appendChild(table);
 
