48cf86f918174239bb963a13715e0f3d0004d3d9
alekko <aleksandrs.kozlovs@mail.com>
2021-02-22 21:47:21 +0200

    Updates

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def datubazes_tests():
 
   return str(lietotaji)
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080) # This line is required to run Flask on repl.it
+
+app.run(host='0.0.0.0', port=8080) # This line is required to run Flask on repl.it
