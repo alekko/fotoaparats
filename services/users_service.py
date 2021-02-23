536d8cc587925b94030e22caab05414768a5f3d3
alekko <aleksandrs.kozlovs@mail.com>
2021-02-23 22:48:56 +0200

    Update users service

--- services/users_service.py
+++ services/users_service.py
@@ -6,20 +6,20 @@ class users_service:
         self.validate_connection()
 
     def validate_connection(self):
-        if db is None: raise Exception('Nevar pieslēgties datubāzei. Pārbaudiet .env vides mainīgo!')
+        if self.db is None: raise Exception('Nevar pieslēgties datubāzei. Pārbaudiet .env vides mainīgo!')
 
     def get_users(self):
-        return db.get('users', [])
+        return self.db.get('users', [])
 
     def add_user(self, new_user):
-        existing_users = db.get('users', [])
+        existing_users = self.db.get('users', [])
 
         new_user['id'] = len(existing_users) + 1
 
-        db['users'] = existing_users + [new_user]
+        self.db['users'] = existing_users + [new_user]
 
     def delete_user(self, user_id):
-        existing_users = db.get('users', [])
+        existing_users = self.db.get('users', [])
         left_users = []
 
         for user in existing_users:
@@ -32,6 +32,6 @@ class users_service:
             user['id'] = new_index
             new_index += 1
 
-        db['users'] = left_users
+        self.db['users'] = left_users
 
 
